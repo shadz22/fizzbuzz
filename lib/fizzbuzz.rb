64699c91360a65cce39e420c2fb2db977d6e751c
Niki <niki.manoledaki@gmail.com>
2019-12-04 15:37:09 +0000

    Adds fizzbuzz method for Integer class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -7,3 +7,15 @@ def fizzbuzz(number)
     "buzz"
   end
 end
+
+class Integer 
+  def fizzbuzz
+      if self % 3  == 0  && self % 5 == 0
+        "fizzbuzz"
+      elsif self % 3 == 0
+        "fizz"
+      elsif self % 5 == 0
+        "buzz"
+      end
+  end
+end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -35,3 +35,21 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(30)).to eq 'fizzbuzz'
   end 
 end
+
+describe 'fizzbuzz' do
+  it 'returns "fizz" for the number 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "buzz" for the number 5' do
+    expect(5.fizzbuzz).to eq 'buzz'
+  end
+end
+
+describe 'fizzbuzz' do
+  it 'returns "fizzbuzz" for the number 15' do
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
+  end
+end
